13ac280e514580d3440749d41adffba5289fdec5
Harsh <harshpxv@gmail.com>
2025-06-08 23:33:50 -0400

    feat: creating the views in a folder

--- backend/api/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-
-# Create your views here.
-def hello_world(request):
-    return HttpResponse("Hello world!")
---
+++ backend/api/views/__init__.py
@@ -0,0 +1,1 @@
+from .task_list_views import TaskListListCreateView, TaskListDetailView
