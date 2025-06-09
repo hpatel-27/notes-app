8502c9ed170aef86678543086a3699f5d42e2d4f
Harsh <harshpxv@gmail.com>
2025-06-08 22:15:27 -0400

    feat: api paths for task lists

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,11 @@
-from . import views
+from .views import TaskListDetailView, TaskListListCreateView
 from django.urls import path
 
 urlpatterns = [
-    path("hello/", views.hello_world, name="hello"),
+    path("tasks/", TaskListListCreateView.as_view(), name="tasks_list"),
+    path(
+        "tasks/<int:task_list_id>",
+        TaskListDetailView.as_view(),
+        name="task_lists_details",
+    ),
 ]
